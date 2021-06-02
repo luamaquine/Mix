e96d2c027fe7c500d1bd27d614360f60bb0b3046
Luã Maury Maquiné da Silva <lmmds.snf20@uea.edu.br>
2021-06-02 15:29:50 -0400

    Menu with game

--- mix_game.py
+++ mix_game.py
@@ -1,8 +1,8 @@
-import pygame
+import pygame, os
 import random
 from pygame.locals import *
 from pygame.time import delay
-import Mix
+from sys import exit
 
 # Helper functions
 def on_grid_random():
@@ -19,18 +19,22 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-screen = pygame.display.set_mode((600, 600))
+menu_selecao = 1
+WIDTH = 600
+HEIGHT = 600
+FPS = 30
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+screen_game = pygame.Surface((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('MIX')
+botao_enter = False
+BG = pygame.transform.scale(pygame.image.load(os.path.join('assets/Background/background-black.png')), (WIDTH, HEIGHT))
 
-def mix_game():
-    pygame.init()
-
-
-    screen = pygame.display.set_mode((600, 600))
-    pygame.display.set_caption('MIX')
-
-    # Ball
-
+# Ball
+def mixgame(screen):
     ball = [(400, 500)]
     ballpos = pygame.Surface((10, 10))
     ballpos.fill((255, 255, 255))
@@ -49,7 +53,7 @@ def mix_game():
 
     snake = [(200, 200), (210, 200), (220, 200)]
     snake_skin = pygame.Surface((10, 10))
-    snake_skin.fill((34, 139, 34)) #White
+    snake_skin.fill((34, 139, 34)) 
 
     apple_pos = on_grid_random()
     apple = pygame.Surface((10,10))
@@ -95,6 +99,7 @@ def mix_game():
     score = 0
     font = pygame.font.Font('freesansbold.ttf', 18)
     level = 1
+
 
     game_over = False
     while not game_over:
@@ -279,17 +284,138 @@ def mix_game():
 
         pygame.display.update()
 
-while True:
-    mix_game()
-    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
-    game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
-    game_over_rect = game_over_screen.get_rect()
-    game_over_rect.midtop = (600 / 2, 200)
-    screen.blit(game_over_screen, game_over_rect)
-    pygame.display.update()
-    pygame.time.wait(500)
     while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                exit()
+        
+        game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+        game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
+        game_over_rect = game_over_screen.get_rect()
+        game_over_rect.midtop = (600 / 2, 200)
+        screen.blit(game_over_screen, game_over_rect)
+        pygame.display.update()
+        pygame.time.wait(500)
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+def eventos():
+
+    global menu_selecao, botao_enter
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:
+                menu_selecao += 1
+            if event.key == K_UP:
+                menu_selecao -= 1
+            if event.key == K_RETURN:
+                menu_selecao += 10
+
+def selecao():
+
+    global menu_selecao, botao_enter
+
+    if menu_selecao == 1:
+
+        screen_game.blit(BG, (0, 0))
+
+        fonte = pygame.font.SysFont ("arial", 20, False, False)
+        start_game = fonte.render(">> Start <<", True, (255, 255, 255))
+        screen_game.blit(start_game, ((WIDTH/2)-60, (HEIGHT/2)))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        credits_game = fonte.render("  Credits  ", True, (255, 255, 255))
+        screen_game.blit(credits_game, ((WIDTH / 2) - 50, (HEIGHT / 2)+22))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        exit_game = fonte.render("    Exit    ", True, (255, 255, 255))
+        screen_game.blit(exit_game, ((WIDTH / 2) - 50, (HEIGHT / 2)+44))
+
+
+    if menu_selecao == 2:
+
+        screen_game.blit(BG, (0, 0))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        start_game = fonte.render("     Start     ", True, (255, 255, 255))
+        screen_game.blit(start_game, ((WIDTH / 2) - 60, (HEIGHT / 2)))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        credits_game = fonte.render(">>Credits<<", True, (255, 255, 255))
+        screen_game.blit(credits_game, ((WIDTH / 2) - 60, (HEIGHT / 2) + 22))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        exit_game = fonte.render("    Exit    ", True, (255, 255, 255))
+        screen_game.blit(exit_game, ((WIDTH / 2) - 50, (HEIGHT / 2) + 44))
+
+    if menu_selecao == 3:
+
+        screen_game.blit(BG, (0, 0))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        start_game = fonte.render("     Start     ", True, (255, 255, 255))
+        screen_game.blit(start_game, ((WIDTH/2)-60, (HEIGHT/2)))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        credits_game = fonte.render("  Credits  ", True, (255, 255, 255))
+        screen_game.blit(credits_game, ((WIDTH / 2) - 50, (HEIGHT / 2)+22))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        exit_game = fonte.render(">>Exit<<", True, (255, 255, 255))
+        screen_game.blit(exit_game, ((WIDTH / 2) - 50, (HEIGHT / 2)+44))
+
+    if menu_selecao == 4:
+        menu_selecao = 3
+
+    if menu_selecao == 0:
+        menu_selecao = 1
+
+    if menu_selecao == 11:
+        mixgame(screen)
+
+    if menu_selecao == 21:
+        menu_selecao = 1
+
+    if menu_selecao == 12:
+        menu_selecao = 200
+
+    if menu_selecao == 200:
+
+        screen_game.blit(BG, (0, 0))
+
+        fonte = pygame.font.SysFont("arial", 20, False, False)
+        fonte_render = fonte.render("----Luã Maury Maquiné da Silva----", True, (100, 245, 250))
+        fonte_render2 = fonte.render("---https://github.com/luamaquine---", True, (247, 142, 67))
+        voltar_render = fonte.render(">> Voltar <<", True, (255, 255, 255))
+        screen_game.blit(fonte_render, ((WIDTH / 2) - 130, (HEIGHT / 2)))
+        screen_game.blit(fonte_render2, ((WIDTH / 2) - 130, (HEIGHT / 2) + 20))
+        screen_game.blit(voltar_render, ((WIDTH / 2) - 45, (HEIGHT / 2) + 300))
+
+    if menu_selecao == 201:
+        menu_selecao = 200
+    if menu_selecao == 199:
+        menu_selecao = 200
+
+    if menu_selecao == 210:
+        menu_selecao = 2
+
+    if menu_selecao == 13:
+        menu_selecao = 500
+
+    if menu_selecao == 500:
+        pygame.quit()
+        exit() 
+while True:
+    selecao()
+    print(menu_selecao)
+    clock.tick(FPS)
+    eventos()
+    screen.blit(screen_game, (0, 0))
+
+    pygame.display.update()
+    pygame.display.flip()
+
